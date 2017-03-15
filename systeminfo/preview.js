b18350a06ed09cce320fbc2fba2f3a3ce0a04e91
fangeel <fangleen@gmail.com>
2017-03-15 14:33:01 +0800

    Fix bug when scan huge size(300+) records

--- systeminfo/preview.js
+++ systeminfo/preview.js
@@ -5,10 +5,53 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const params = {
   TableName: process.env.DYNAMODB_TABLE,
+  ProjectionExpression: "createdAt, id, serial, #data.jvmMemInfo, #data.freeInfo.#total," +
+    "#data.freeInfo.used, #data.freeInfo.#free, #data.systemTime, #data.topInfo.processes",
+  ExpressionAttributeNames: {
+    "#data": "data",
+    "#total": "total",
+    "#free": "free",
+  },
 };
 
 module.exports.preview = (event, context, callback) => {
-  // fetch all todos from the database
+  scanExecute(callback);
+};
+
+var sendResponse = (err, items, callback) => {
+  // create a response
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(items.map(record => {
+      return {
+        createdAt: record.createdAt,
+        id: record.id,
+        serial: record.serial,
+        data: {
+          jvmMemInfo: {
+            freeMem: record.data.jvmMemInfo.freeMem,
+            inUseMem: record.data.jvmMemInfo.inUseMem,
+            coreNum: record.data.jvmMemInfo.coreNum,
+            maxMem: record.data.jvmMemInfo.maxMem
+          },
+          freeInfo: {
+            total: record.data.freeInfo.total,
+            used: record.data.freeInfo.used,
+            free: record.data.freeInfo.free
+          },
+          topInfo: {
+            processes: record.data.topInfo.processes
+          },
+          systemTime: record.data.systemTime
+        }
+      }
+    })),
+  };
+  callback(null, response);
+};
+
+var items = [];
+var scanExecute = (callback) => {
   dynamoDb.scan(params, (error, result) => {
     // handle potential errors
     if (error) {
@@ -17,34 +60,12 @@ module.exports.preview = (event, context, callback) => {
       return;
     }
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Items.map(record => {
-        return {
-          createdAt: record.createdAt,
-          id: record.id,
-          serial: record.serial,
-          data: {
-            jvmMemInfo: {
-              freeMem: record.data.jvmMemInfo.freeMem,
-              inUseMem: record.data.jvmMemInfo.inUseMem,
-              coreNum: record.data.jvmMemInfo.coreNum,
-              maxMem: record.data.jvmMemInfo.maxMem
-            },
-            freeInfo: {
-              total: record.data.freeInfo.total,
-              used: record.data.freeInfo.used,
-              free: record.data.freeInfo.free
-            },
-            topInfo: {
-              processes: record.data.topInfo.processes
-            },
-            systemTime: record.data.systemTime
-          }
-        }
-      })),
-    };
-    callback(null, response);
+    items = items.concat(result.Items);
+    if (result.LastEvaluatedKey) {
+      params.ExclusiveStartKey = result.LastEvaluatedKey;
+      scanExecute(callback);
+    } else {
+      sendResponse(error, items, callback);
+    }
   });
 };
